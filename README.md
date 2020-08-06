debb15b25a7d31b8e65a94abd5114d0479ad78bd
JohnJB <neardem1@gmail.com>
2020-08-06 18:44:23 +0100

    first commit with README.md this is a repo for AWS and the connection

---
+++ README.md
@@ -0,0 +1,12 @@
+# AWS intro and Deployment
+
+
+### What is AWS?
+
+### Useful commands --
+
+```
+scp -i ~/.ssh/DevOpsStudents.pem -r app/ ubuntu@3.250.67.195:~/app/
+```
+
+## Steps to setting up a EMC2 Machine
---
+++ app/provision.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#update
+sudo apt-get update
+
+# Install Nginx
+sudo apt-get install nginx
+
+# Install NodeJS and NPM installer
+sudo apt-get install nodejs
+sudo apt-get install npm
+
+scp -i ~/.ssh/DevOpsStudents.pem -r app/ ubuntu@3.250.67.195:~/app/
+
+sudo apt-get upgrade 
+
+sudo software-properties -y    
+
+sudo apt-get install python-software-properties -y 
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+sudo apt-get install nodejs -y 
+
+sudo npm install
+
+sudo node app.js
